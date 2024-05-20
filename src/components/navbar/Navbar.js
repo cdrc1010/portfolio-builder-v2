5e06bee8960bfa7c3e7b243bb6f9487a5682bfec
cdrc1010 <cdrcgurimbao22@gmail.com>
2024-05-20 14:12:39 +0800

    Create Route for Public Route to access the portfolio without logging in

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,51 +1,123 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import styles from './Navbar.module.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
 
-import { useLogout } from '../../hooks/useLogout';
-import { useAuthContext } from '../../hooks/useAuthContext';
+import { useLogout } from "../../hooks/useLogout";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
-const Navbar = () => {
+const Navbar = ({ visibleInPublic }) => {
+  const { user } = useAuthContext();
+  const { logout } = useLogout();
 
-    const { user } = useAuthContext()
-    const {logout} = useLogout()
+  const scrollTo = (id) => {
+    const sectionId = document.getElementById(id);
+    sectionId.scrollIntoView({ behavior: "smooth" });
+  };
 
-    const scrollTo = (id) => {
-        const sectionId = document.getElementById(id)
-        sectionId.scrollIntoView({ behavior: 'smooth' });
-    }
+  const renderElement = () => {
+    return user || visibleInPublic ? (
+      <>
+        <li
+          onClick={() => {
+            scrollTo("profile");
+          }}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => {
+            scrollTo("about");
+          }}
+        >
+          About
+        </li>
+        <li
+          onClick={() => {
+            scrollTo("skills");
+          }}
+        >
+          Skills
+        </li>
+        <li
+          onClick={() => {
+            scrollTo("projects");
+          }}
+        >
+          Projects
+        </li>
+        <li
+          onClick={() => {
+            scrollTo("contact");
+          }}
+        >
+          Contact
+        </li>
+        {<li onClick={logout}>Logout</li>}
 
-    const renderElement = () => {
-        return user ?
-           ( <>
-
-                <li onClick={() => {scrollTo('profile')}}>Home</li>
-                <li onClick={() => {scrollTo('about')}}>About</li>
-                <li onClick={() => {scrollTo('skills')}}>Skills</li>
-                <li onClick={() => {scrollTo('projects')}}>Projects</li>
-                <li onClick={() => {scrollTo('contact')}}>Contact</li>
-                <li onClick={logout}>Logout</li>
-
-                {/* <li><button className='btn' onClick={() => {scrollTo('about')}}>About</button></li>
+        {/* <li><button className='btn' onClick={() => {scrollTo('about')}}>About</button></li>
                 <li><button className='btn'onClick={() => {scrollTo('skills')}}>Skills</button></li>
                 <li><button className='btn'onClick={() => {scrollTo('projects')}}>Projects</button></li>
                 <li ><button onClick={logout}>Logout</button></li> */}
-            </>)
-            :
-           ( <>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/signup">Signup</Link></li>
-            </>)
-    }
+      </>
+    ) : (
+      <>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+      </>
+    );
+  };
 
-    return (
-        <nav className={styles.navbar}>
-            <ul>
-                {renderElement()}
-            </ul>
-            
-        </nav>
-    )
-}
 
-export default Navbar
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        {!visibleInPublic && renderElement()}
+        {visibleInPublic && (
+          <>
+            <li
+              onClick={() => {
+                scrollTo("profile");
+              }}
+            >
+              Home
+            </li>
+            <li
+              onClick={() => {
+                scrollTo("about");
+              }}
+            >
+              About
+            </li>
+            <li
+              onClick={() => {
+                scrollTo("skills");
+              }}
+            >
+              Skills
+            </li>
+            <li
+              onClick={() => {
+                scrollTo("projects");
+              }}
+            >
+              Projects
+            </li>
+            <li
+              onClick={() => {
+                scrollTo("contact");
+              }}
+            >
+              Contact
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
