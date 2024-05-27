b43e5efb8a9fba778231c60c61ab5c091ae11b2c
cdrc1010 <cdrcgurimbao22@gmail.com>
2024-05-27 22:42:07 +0800

    lmnt-4: add a styling for responsiveness of web application

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 import { useLogout } from "../../hooks/useLogout";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { motion } from 'framer-motion';
+import { useScreenSize } from "../../hooks/useScreenSize";
 
 const Navbar = ({ visibleInPublic }) => {
   const { user } = useAuthContext();
   const { logout } = useLogout();
+  const screenSize = useScreenSize();
+  const isMobile = screenSize.width <= 768
+
+  console.log('screenSize: ', screenSize)
+  const [toggle, setToggle] = useState(false)
 
   const scrollTo = (id) => {
     const sectionId = document.getElementById(id);
@@ -18,8 +24,6 @@ const Navbar = ({ visibleInPublic }) => {
   const publicNavbar = ["Profile", "About", "Skills", "Projects", "Contact"]
 
   const userNavBar = publicNavbar.concat('Logout')
-  console.log('userNavBar: ', userNavBar)
-  console.log('user: ', user)
 
   const handleNavItemClick = item => {
     if (item === 'Logout') {
@@ -31,49 +35,13 @@ const Navbar = ({ visibleInPublic }) => {
   const renderElement = () => {
     return user ? (
       <>
-        {/* <li
-          onClick={() => {
-            scrollTo("profile");
-          }}
-        >
-          Home
-        </li>
-        <li
-          onClick={() => {
-            scrollTo("about");
-          }}
-        >
-          About
-        </li>
-        <li
-          onClick={() => {
-            scrollTo("skills");
-          }}
-        >
-          Skills
-        </li>
-        <li
-          onClick={() => {
-            scrollTo("projects");
-          }}
-        >
-          Projects
-        </li>
-        <li
-          onClick={() => {
-            scrollTo("contact");
-          }}
-        >
-          Contact
-        </li>
-        <li onClick={logout}>Logout</li> */}
         {userNavBar.map((el, idx) => (
           <motion.li onClick={handleNavItemClick(el)} key={idx}
             initial={{ opacity: 0, x: -50 }}
             whileInView={{ opacity: 1, x: 0, transition: { delay: 0.3 * idx, duration: .2 } }}
             viewport={{ once: true, amount: 0.5 }}
           >
-            {el}  
+            {el}
           </motion.li>
         )
         )}
@@ -93,45 +61,13 @@ const Navbar = ({ visibleInPublic }) => {
 
   return (
     <nav className={styles.navbar}>
-      <motion.ul initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ type: 'spring', stiffness: 180, duration: 1.5 }}>
+      {isMobile && <div className={styles.hamburger} onClick={() => setToggle(!toggle)}>
+        &#9776;
+      </div>}
+      {(!isMobile || toggle) && <motion.ul initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ type: 'spring', stiffness: 180, duration: 1.5 }}>
         {!visibleInPublic && renderElement()}
         {visibleInPublic && (
           <>
-            {/* <li
-              onClick={() => {
-                scrollTo("profile");
-              }}
-            >
-              Home
-            </li>
-            <li
-              onClick={() => {
-                scrollTo("about");
-              }}
-            >
-              About
-            </li>
-            <li
-              onClick={() => {
-                scrollTo("skills");
-              }}
-            >
-              Skills
-            </li>
-            <li
-              onClick={() => {
-                scrollTo("projects");
-              }}
-            >
-              Projects
-            </li>
-            <li
-              onClick={() => {
-                scrollTo("contact");
-              }}
-            >
-              Contact
-            </li> */}
             {publicNavbar.map((el, idx) => (
               <motion.li onClick={() => { scrollTo(el.toLowerCase()) }} key={idx}
                 initial={{ opacity: 0, x: -50 }}
@@ -143,7 +79,8 @@ const Navbar = ({ visibleInPublic }) => {
             ))}
           </>
         )}
-      </motion.ul>
+        <hr />
+      </motion.ul>}
     </nav>
   );
 };
