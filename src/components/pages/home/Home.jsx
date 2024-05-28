bdc607d868ceb4cfc6330cccaf67264dfa270eee
cdrc1010 <cdrcgurimbao22@gmail.com>
2024-05-28 10:20:31 +0800

    lmnt-5: add a function that fires when user logged in to save the data in public collection and update the page title and icon

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import About from "./About";
 import { useAuthContext } from "../../../hooks/useAuthContext";
 import { useCollection } from "../../../hooks/useCollection";
 import Portfolio from "./Portfolio";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
+function changeFavicon(src) {
+  let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+  link.type = 'image/x-icon';
+  link.rel = 'shortcut icon';
+  link.href = src;
+
+  if (!link.parentNode) {
+    document.head.appendChild(link);
+  }
+}
+
+
 const Home = ({ visibleInPublic }) => {
   const { user } = useAuthContext();
   const params = useParams();
+  const [pageTitle, setPageTitle] = useState('');
+
   const userUID = params.id ? params.id : user.uid;
   const { documents, error, loading } = useCollection("userProfile", [
     "uid",
@@ -20,6 +34,14 @@ const Home = ({ visibleInPublic }) => {
   const {name, photoURL, email} = documentObject
   const img = user ? user.photoURL : photoURL
 
+  useEffect(() => {
+    if (publicDocument || user) {
+      setPageTitle(name)
+      document.title = pageTitle;
+      changeFavicon(img);
+    }
+  }, [pageTitle, publicDocument]);
+
   const renderContent = () => {
     return (
       <>
@@ -29,8 +51,8 @@ const Home = ({ visibleInPublic }) => {
             currentUser={user}
             profilePic={img}
             visibleInPublic={visibleInPublic}
-            displayName ={name}
-            publicEmail = {email}
+            displayName={name}
+            publicEmail={email}
           />
         ) : (
           <About uid={userUID} />
